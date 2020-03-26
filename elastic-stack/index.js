00863a6a9d38ba0cbeed0960260a02ced04874b8
bankuoniichan <colkeover@gmail.com>
2020-03-26 14:40:49 +0700

    Add chat simulate function

--- elastic-stack/index.js
+++ elastic-stack/index.js
@@ -1,12 +1,87 @@
-const pino = require('pino')();
+const pino = require("pino")();
 
-const clients = ["142.27.6.41","142.27.6.44","142.52.14.3","142.52.19.34","25.103.90.4"];
+const delay = ms => {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+};
 
-clients.forEach((client,index) => {
-	pino.info(JSON.stringify({
-		client: client,
-		question: `Question ${index%2?'B':'A'}`,
-		answer: index%2?null:'Answer A',
-		fallback: index%2>0
-	}))
-})
+const QAMapping = {};
+[..."ABCBDEF"].forEach(char => {
+  QAMapping[`Question ${char}`] = `Answer ${char}`;
+});
+
+const getRandomInteger = (n = 100) => {
+  const randomValue = Math.random();
+  const randomInt = Math.floor(n * randomValue);
+  return randomInt;
+};
+
+const getRandomInterval = () => {
+  const minInterval = 1000; // 1 second
+  const maxInterval = 10 * 1000; // 10 second
+  const diff = maxInterval - minInterval;
+  const randomInterval = minInterval + getRandomInteger(diff);
+  return randomInterval;
+};
+
+const getRandomQuestion = () => {
+  const questions = Object.keys(QAMapping);
+  const randomQuestionIndex = getRandomInteger(questions.length);
+  return questions[randomQuestionIndex];
+};
+
+const answerQuestion = question => {
+  return QAMapping[question] || "Fallback Answer";
+};
+
+const getRandomDecision = (rate = 0.5) => {
+  return Math.random() >= rate;
+};
+
+const getNewSession = (function() {
+  let sessionId = 1;
+  return () => {
+    return sessionId++;
+  };
+})();
+
+const simulateConversations = clients => {
+  clients.forEach(client => {
+    simulateConversation(client);
+  });
+};
+
+const postMessage = (sessionId, actor, message) => {
+  pino.info(
+    JSON.stringify({
+      sessionId,
+      actor,
+      message
+    })
+  );
+};
+
+const simulateConversation = async client => {
+  do {
+    await delay(getRandomInterval()); // simulate interval between session
+    const sessionId = getNewSession();
+    let nQuestionToAsk = 1 + getRandomInteger(3); // 1 to 3 questions
+    while (nQuestionToAsk--) {
+      const question = getRandomQuestion();
+      postMessage(sessionId, client, question);
+      await delay(500); // simulate server response delay
+      postMessage(sessionId, "kaitomm", answerQuestion(question));
+      await delay(1500); // simulate user action delay
+    }
+  } while (getRandomDecision()); // randomly decide to start new session
+};
+
+const clients = [
+  "142.27.6.41",
+  "142.27.6.44",
+  "142.52.14.3",
+  "142.52.19.34",
+  "25.103.90.4"
+];
+simulateConversations(clients);
