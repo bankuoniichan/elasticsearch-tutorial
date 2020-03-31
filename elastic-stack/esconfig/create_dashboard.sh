ff49e25736156b0e458a31b42172b75a37c016fb
bankuoniichan <colkeover@gmail.com>
2020-03-31 13:28:01 +0700

    Fix wrong shell script syntax

--- elastic-stack/esconfig/create_dashboard.sh
+++ elastic-stack/esconfig/create_dashboard.sh
@@ -9,7 +9,7 @@ while [[ "$(
     -H 'kbn-xsrf:true' -H 'Content-type:application/json' \
     --write-out "%{http_code}" --output /dev/null --silent \
     --data @dashboard.json
-)" != "200"]]
+)" != "200" ]]
 do
     sleep 10
     echo Retrying to create dashboard...
