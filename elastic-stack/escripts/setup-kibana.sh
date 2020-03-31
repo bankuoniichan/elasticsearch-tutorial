7132ea50f465078a4f00ba0d4a38533b0b3f2213
bankuoniichan <colkeover@gmail.com>
2020-03-31 11:19:00 +0700

    Add plugin & default dashboard

--- elastic-stack/escripts/setup-kibana.sh
+++ elastic-stack/escripts/setup-kibana.sh
@@ -18,3 +18,15 @@ do
     sleep 2
     echo Retrying...
 done
+
+# Create Dashboard
+while [[ "$(
+    curl -XPOST localhost:5601/api/kibana/dashboards/import \
+    -H 'kbn-xsrf:true' -H 'Content-type:application/json' \
+    --write-out "%{http_code}" --output /dev/null --silent \
+    --data @dashboard.json
+)" != "200"]]
+do
+    sleep 2
+    echo Retrying to create dashboard...
+done
