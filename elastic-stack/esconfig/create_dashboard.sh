64f7f47acfb2f4fefaf7ec5cd48f0f5a8d800949
bankuoniichan <colkeover@gmail.com>
2020-03-31 12:22:10 +0700

    Change way to add dashboard

---
+++ elastic-stack/esconfig/create_dashboard.sh
@@ -0,0 +1,12 @@
+# Try to send import request every 10 second
+# Both "success" and "already exist" responses have HTTP status of 200
+while [[ "$(
+    curl -XPOST localhost:5601/api/kibana/dashboards/import \
+    -H 'kbn-xsrf:true' -H 'Content-type:application/json' \
+    --write-out "%{http_code}" --output /dev/null --silent \
+    --data @dashboard.json
+)" != "200"]]
+do
+    sleep 10
+    echo Retrying to create dashboard...
+done
